12e630902e2d03a7f6050258b568ff19b4055cb3
Anselm Prasad <madushanp685@gmail.com>
2025-10-06 15:25:04 +0530

    Update Gradle and Dart SDK versions; enhance network timeout settings and adjust package versions in pubspec.lock

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,11 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+plugins {
+    // Update the Android plugin version to match the classpath (8.7.0)
+    id("com.android.application") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
+}
+
 allprojects {
     repositories {
         google()
@@ -5,17 +13,17 @@ allprojects {
     }
 }
 
+// Move build outputs to a common folder
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// Clean task
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
